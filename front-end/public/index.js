74ec7ff0b029376a90d7226f1acfa143ea70a266
Cristobal Grau <cristobalgrau@gmail.com>
2024-02-12 21:19:30 +0000

    upload front-end files

---
+++ front-end/public/index.js
@@ -0,0 +1,24 @@
+// Javascript code for views counter
+// ----------------------------------
+
+// Selects the HTML element with the class 'counter-number'
+const counter = document.querySelector(".counter-number");
+
+// Function to update the counter
+function updateCounter() {
+    // Makes an HTTP GET request to the provided AWS Lambda Function
+    fetch("https://co6zhqpuof.execute-api.us-east-1.amazonaws.com/cv/views")
+        // Handles the response by converting the response body to JSON format
+        .then(response => response.json())
+        .then(data => {
+            // Updates the content of the HTML element with the retrieved data
+            counter.innerHTML = ` Views: ${data}`;
+        })
+        // Catches and handles any errors that may occur during the request or data processing
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+}
+
+// Calls the function to update the counter when the page loads
+updateCounter();
